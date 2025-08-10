81afbd7d9f61d2c06036d1c96aed579c755aa093
lovely-alonso <lovely.alonso0501@gmail.com>
2025-08-10 21:17:03 +0800

    updated

--- src/alonso/banking/bankingClass.java
+++ src/alonso/banking/bankingClass.java
@@ -3,26 +3,50 @@ package alonso.banking;
 import java.util.Scanner;
 
 public class bankingClass {
-     int accountNo = 10001;
-    String name;
-    float initialBalance = 0;
-    int pin = 1234;
-    
-    public boolean verifyAccount(int acc, int pn){
-        
-            if(acc == accountNo && pn == pin){
-                return true;
-            }else{
-                return false;
-            }
+     private int accountNo;
+    private float balance;
+    private int pin;
+
+    public void setAccountNo(int accountNo) {
+        this.accountNo = accountNo;
     }
-    
-    public int setAccount(){
+
+    public int getAccountNo() {
         return accountNo;
     }
-    
-    public void viewBalance(){
-    
-    
+
+    public void setPin(int pin) {
+        this.pin = pin;
     }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public boolean verifyAccount(int acc, int pn) {
+        return (acc == accountNo && pn == pin);
+    }
+
+    public void deposit(float amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid amount!");
+        }
+    }
+
+    public void withdraw(float amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid amount!");
+        }
+    }
+
+    public void viewBalance() {
+        System.out.println("Current Balance: " + balance);
+    }
+
 }
--- src/alonso/main.java
+++ src/alonso/main.java
@@ -5,50 +5,115 @@ import alonso.banking.bankingClass;
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {
-          Scanner sc = new Scanner(System.in);
-        
-        System.out.println("HELLO WELCOME TO THE SYSTEM!");
-        System.out.println("What do you feel doing today? ");
-        System.out.println("1. Banking");
-        System.out.println("2. Doctors Appointment");
-        System.out.println("3. Shopping");
-        
-        System.out.print("Enter choice: ");
-        int choice = sc.nextInt();
-        
-        int i=3;
-        while(i>0){
-        switch(choice){
-           case 1:
-                  bankingClass bc = new bankingClass();
-                
-                System.out.println("Enter your Account No: ");
-                int accountNo = sc.nextInt();
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-                System.out.println("Enter your Pin: ");
-                int pin = sc.nextInt();
-              
-                    if(bc.verifyAccount(accountNo, pin)){
-                        System.out.println("LOGIN SUCCESS");
-                    }else{
-                        i--;
-                        System.out.println("INVALID ACCOUNT! you have " + i + " left");
+        System.out.println("---WELCOME TO ALONSO'S SYSTEM---");
+        System.out.println("What do you feel like doing today?");
+        System.out.println("1. Banking");
+        System.out.println("2. Shopping");
+        System.out.println("3. Pay Bills");
+
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                bankingClass[] bApp = new bankingClass[10];
+                bApp[0] = new bankingClass();
+
+                int response;
+                do {
+                    System.out.println("\n--- Banking Menu ---");
+                    System.out.println("1. Register Account");
+                    System.out.println("2. Sign In Account");
+                    System.out.println("3. View All Accounts");
+                    System.out.print("Enter Selection: ");
+                    int action = sc.nextInt();
+
+                    switch (action) {
+                        case 1:
+                            System.out.print("Enter Account No.: ");
+                            bApp[0].setAccountNo(sc.nextInt());
+                            System.out.print("Set Account Pin: ");
+                            bApp[0].setPin(sc.nextInt());
+                            break;
+
+                        case 2: 
+                            System.out.print("Enter Account No: ");
+                            int acc = sc.nextInt();
+                            System.out.print("Enter PIN: ");
+                            int pn = sc.nextInt();
+
+                            if (bApp[0].verifyAccount(acc, pn)) {
+                                System.out.println("LOGIN SUCCESS");
+
+                                int transChoice;
+                                do {
+                                    System.out.println("\n--- Transactions Menu ---");
+                                    System.out.println("1. Deposit");
+                                    System.out.println("2. Withdraw");
+                                    System.out.println("3. View Balance");
+                                    System.out.println("0. Logout");
+                                    System.out.print("Enter choice: ");
+                                    transChoice = sc.nextInt();
+
+                                    switch (transChoice) {
+                                        case 1:
+                                            System.out.print("Enter deposit amount: ");
+                                            float depositAmount = sc.nextFloat();
+                                            bApp[0].deposit(depositAmount);
+                                            break;
+                                        case 2:
+                                            System.out.print("Enter withdraw amount: ");
+                                            float withdrawAmount = sc.nextFloat();
+                                            bApp[0].withdraw(withdrawAmount);
+                                            break;
+                                        case 3:
+                                            bApp[0].viewBalance();
+                                            break;
+                                        case 0:
+                                            System.out.println("Logging out...");
+                                            break;
+                                        default:
+                                            System.out.println("Invalid choice!");
+                                    }
+                                } while (transChoice != 0);
+
+                            } else {
+                                System.out.println("LOGIN FAILED");
+                            }
+                            break;
+
+                        case 3:
+                            for (int i = 0; i < bApp.length && bApp[i] != null; i++) {
+                                System.out.println("Account " + (i + 1) + ": " + bApp[i].getAccountNo());
+                            }
+                            break;
+
+                        default:
+                            System.out.println("INVALID SELECTION!");
                     }
-        
+
+                    System.out.print("\nContinue? (1 = Yes / 0 = No): ");
+                    response = sc.nextInt();
+
+                } while (response == 1);
                 break;
-        
-                
+
             case 2:
-                
+                System.out.println("Shopping ni siya na part");
                 break;
+
             case 3:
-                
+                System.out.println("Pay Bills ni siya na part");
                 break;
+
             default:
-                System.out.println("Invalid Selection!");
+                System.out.println("INVALID SELECTION!");
         }
-        }
+
+        sc.close();
     }
-    
+
 }
